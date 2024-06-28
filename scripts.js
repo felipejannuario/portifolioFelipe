218bd4ca2134a97c64b48b608eebbffbdb01d711
FJANNUÁRIO <felipejanuarioalves@outlook.com>
2024-06-28 09:26:24 -0300

    meu portfifolio

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".formulario-fale-comigo")
+const mascara = document.querySelector(".mascara-formulario")
+
+function mostrarForm() {
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderForm() {
+    form.style.left = "-300px";
+    form.style.transform = "translatex(0)";
+    mascara.style.visibility = "hidden";
+}
+
+function ajustarVolume(volume) {
+    const audio = document.getElementById("myAudio");
+    audio.volume = volume;
+}
+
+// Ajuste de volume inicial
+ajustarVolume(0.40); // Define o volume inicial para 20%
+
+function abrirLink() {
+    window.location.href = "https://example.com"; // Altere o link para o destino desejado
+}
+
+function openInNewTab(url) {
+    window.open(url, "_blank");
+}
